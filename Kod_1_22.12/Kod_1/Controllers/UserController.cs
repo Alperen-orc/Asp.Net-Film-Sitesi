b81a4e2b05c2cd698c8be1c1e74454543eeb3c8a
Alperen-orc <82825640+Alperen-orc@users.noreply.github.com>
2022-12-22 00:18:38 +0300

    Asp.Net-Film-Sitesi

---
+++ Kod_1_22.12/Kod_1/Controllers/UserController.cs
@@ -0,0 +1,49 @@
+﻿using Kod_1.Data;
+using Kod_1.Entity;
+using Kod_1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kod_1.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly MovieContext _context;
+        public UserController(MovieContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult CreateUser()
+        {
+            return View(new UserModel());
+        }
+        [HttpPost]
+        public IActionResult CreateUser(UserModel model)
+        {
+            
+
+            if (ModelState.IsValid)
+            {
+                var entity = new User
+                {
+                    UserId = model.UserId,
+                    UserName = model.UserName,
+                    Email=model.Email,
+                    Name = model.Name,
+                    Password = model.Password,
+                    BirthDate = model.BirthDate
+                };
+
+                _context.Users.Add(entity);
+                _context.SaveChanges();
+                return RedirectToAction("MovieList", "Admin");
+            }
+            ViewBag.Genres = _context.Genres.ToList();
+            return View(model);
+        }
+    }
+}
---
+++ Kod_1_22.12/Kod_1/Models/MovieComment.cs
@@ -0,0 +1,17 @@
+﻿using Kod_1.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kod_1.Models
+{
+    public class MovieComment
+    {
+        public Movie Movies { get; set; }
+        public List<Comment> Comments { get; set; }
+        public string comment { get; set; }
+        public string Name { get; set; }
+        public int MovieId { get; set; }
+    }
+}
