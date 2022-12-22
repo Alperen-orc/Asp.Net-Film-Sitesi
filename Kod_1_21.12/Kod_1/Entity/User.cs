5968c71fc1db2be3a46bcc41fbbe4e6a4c639390
Alperen-orc <82825640+Alperen-orc@users.noreply.github.com>
2022-12-22 21:58:17 +0300

    Asp.Net-Film-Sitesi

---
+++ Kod_1_21.12/Kod_1/Entity/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kod_1.Entity
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string RePassword { get; set; }
+        public string Url { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
