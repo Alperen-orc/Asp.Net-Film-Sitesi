403ac42d1d27dffdc89d93985d2e70b9ce0d64c0
Alperen-orc <82825640+Alperen-orc@users.noreply.github.com>
2022-12-27 01:03:14 +0300

    Asp.Net-Film-Sitesi

---
+++ Kod_1_25.12/Kod_1/Migrations/20221224151322_InitialCreate6.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Kod_1.Migrations
+{
+    public partial class InitialCreate6 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Stars",
+                columns: table => new
+                {
+                    StarId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StarValue = table.Column<int>(type: "int", nullable: false),
+                    MovieId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Stars", x => x.StarId);
+                    table.ForeignKey(
+                        name: "FK_Stars_Movies_MovieId",
+                        column: x => x.MovieId,
+                        principalTable: "Movies",
+                        principalColumn: "MovieId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Stars_MovieId",
+                table: "Stars",
+                column: "MovieId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Stars");
+        }
+    }
+}
